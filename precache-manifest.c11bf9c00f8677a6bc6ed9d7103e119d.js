b209222970c672968cbae2ddabc9232e681a1b43
marla729 <carcar2014m@gmail.com>
2020-06-21 08:45:38 -0400

    Updates

---
+++ precache-manifest.c11bf9c00f8677a6bc6ed9d7103e119d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "548fd3dd245b24a117f436742ece7380",
+    "url": "/marla729/marla729.github.io/index.html"
+  },
+  {
+    "revision": "a77da985818dccbca540",
+    "url": "/marla729/marla729.github.io/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "776dca0dbf57e78cba1e",
+    "url": "/marla729/marla729.github.io/static/js/2.2d797b7a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/marla729/marla729.github.io/static/js/2.2d797b7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a77da985818dccbca540",
+    "url": "/marla729/marla729.github.io/static/js/main.83d7a682.chunk.js"
+  },
+  {
+    "revision": "4882266af499b53c55be",
+    "url": "/marla729/marla729.github.io/static/js/runtime-main.2349f043.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/marla729/marla729.github.io/static/media/logo.5d5d9eef.svg"
+  }
+]);
